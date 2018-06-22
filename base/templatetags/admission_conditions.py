13ba753bd6ab2c7f6880b6c6eae2c71db90a6c22
Stéphane Wirtel <stephane@wirtel.be>
2018-06-22 12:48:17 +0200

    Insert a template tag render_condition_rows and use it

--- base/templatetags/admission_conditions.py
+++ base/templatetags/admission_conditions.py
@@ -1,30 +1,55 @@
+import textwrap
+
 from django import template
-from django.forms.utils import flatatt
-from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe
-from django.utils.html import format_html
 
 register = template.Library()
 
 
-def text_value(value):
-    # credits Django-Bootstrap 3
-    # https://github.com/dyve/django-bootstrap3/blob/master/bootstrap3/text.py#L11:5
-    if value is None:
-        return ''
-    return force_text(value)
-
-
-def render_tag(tag, attrs=None, content=None, close=True):
-    # credits Django-Bootstrap 3
-    # https://github.com/dyve/django-bootstrap3/blob/master/bootstrap3/utils.py#L129
-    builder = '<{tag}{attrs}>{content}'
-    if content or close:
-        builder += '</{tag}>'
-    return format_html(builder, tag=tag, attrs=mark_safe(flatatt(attrs)) if attrs else '',
-                       content=text_value(content))
-
-
 @register.simple_tag
-def table_row_admission_line(tag_id, *args, **kwargs):
-    return render_tag('tr', attrs={'id': tag_id})
+def render_condition_rows(section_name, header_text, records):
+    content = [render_header_table(header_text, section_name)]
+
+    for record in records:
+        content.append(render_row(record, section_name))
+
+    return mark_safe(''.join(content))
+
+
+def render_row(record, section_name):
+    row = """
+        <tr id="{section}_{record_id}">
+            <td id="{section}_{record_id}_diploma">{diploma}</td>
+            <td id="{section}_{record_id}_conditions">{conditions}</td>
+            <td id="{section}_{record_id}_access" class="text-right">{access}</td>
+            <td id="{section}_{record_id}_remarks">{remarks}</td>
+            <td class="text-right">{record_id} -
+                <a href="#" role="button" data-record-id="{record_id}" data-section="{section}" class="button line-remove-btn">
+                    <span class="glyphicon glyphicon-remove-circle" style="font-size: 16px;" aria-hidden="true">
+                    </span>
+                </a>
+                <a href="#" role="button" class="button line-edit-btn" data-record-id="{record_id}" data-section="{section}">
+                    <span class="glyphicon glyphicon-edit" style="font-size: 16px;" aria-hidden="true"></span>
+                </a>
+            </td>
+        </tr>
+    """.format(section=section_name,
+               record_id=record.id,
+               diploma=mark_safe(record.diploma),
+               conditions=mark_safe(record.conditions),
+               access=mark_safe(record.access),
+               remarks=mark_safe(record.remarks))
+    return textwrap.dedent(row)
+
+
+def render_header_table(header_text, section_name):
+    header = """
+        <tr id="{section_name}">
+            <td colspan="5" class="info">
+                {header_text} <a class="button line-add-btn" data-section="{section_name}" href="#">
+                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                </a>
+            </td>
+        </tr>
+    """.format(section_name=section_name, header_text=header_text)
+    return textwrap.dedent(header)
