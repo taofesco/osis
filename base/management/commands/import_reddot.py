89c01d01b5d2807af9a6f6c99908a9290cdd46bb
Stéphane Wirtel <stephane@wirtel.be>
2018-06-28 18:02:58 +0200

    code climate compliant

--- base/management/commands/import_reddot.py
+++ base/management/commands/import_reddot.py
@@ -263,8 +263,10 @@ class Command(BaseCommand):
             'access' + self.lang: line['access'],
             'remarks' + self.lang: line['remarks']
         }
-        # import pdb; pdb.set_trace()
-        queryset = AdmissionConditionLine.objects.filter(section=line['title'], admission_condition=admission_condition, external_id=line['external_id'])
+
+        queryset = AdmissionConditionLine.objects.filter(section=line['title'],
+                                                         admission_condition=admission_condition,
+                                                         external_id=line['external_id'])
         if not queryset.count():
             AdmissionConditionLine.objects.create(
                 section=line['title'],
@@ -279,7 +281,6 @@ class Command(BaseCommand):
             setattr(acl, 'access' + self.lang, line['access'])
             setattr(acl, 'remarks' + self.lang, line['remarks'])
             acl.save()
-
 
     def save_text_of_conditions(self, admission_condition, item):
         texts = item['info'].get('texts', {}) or {}
